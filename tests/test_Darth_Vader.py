179ab845915abc02f50ecbd63ec241af3f16129b
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-05-05 19:42:19 +0300

    Initiation project

---
+++ tests/test_Darth_Vader.py
@@ -0,0 +1,57 @@
+import json
+from utils.cheking import Checking
+from utils.api import StarWarsApi
+
+
+"""Uploading a list of characters from films with Darth Vader to a file"""
+class TestLoadListCharacters:
+    def test_upload_list(self):
+
+        # print("Method POST")
+        # result_post = GoogleMapsApi.create_new_place()
+        # check_post = result_post.json()
+        # place_id = check_post.get("place_id")
+        # Checking.check_status_code(result_post, 200)
+        # Checking.check_json_token(result_post, ['status', 'place_id', 'scope', 'reference', 'id'])
+        # Checking.check_json_value(result_post, 'status', 'OK')
+
+        print("Method GET")
+        people = '/people/4/'
+        result_get = StarWarsApi.get_upload_character(people)
+        Checking.check_status_code(result_get, 200)
+        # token = json.loads(result_get.text)   #  getting a list of fields
+        # print(list(token))
+        # Checking.check_json_token(result_get, ['location', 'accuracy', 'name', 'phone_number', 'address',
+        #                                        'types', 'website', 'language'])
+        # Checking.check_json_value(result_get, 'address', '29, side layout, cohen 09')
+
+        # print("Method PUT")
+        # result_put = GoogleMapsApi.put_new_place(place_id)
+        # Checking.check_status_code(result_put, 200)
+        # Checking.check_json_token(result_put, ['msg'])
+        # Checking.check_json_value(result_put, 'msg', 'Address successfully updated')
+        #
+        #
+        # print("Method GET PUT")
+        # result_get = GoogleMapsApi.get_new_place(place_id)
+        # Checking.check_status_code(result_get, 200)
+        # Checking.check_json_token(result_get, ['location', 'accuracy', 'name', 'phone_number', 'address',
+        #                                        'types', 'website', 'language'])
+        # Checking.check_json_value(result_get, 'address', '100 Lenina street, RU')
+        #
+        # print("Method DELETE")
+        # result_delete = GoogleMapsApi.delete_new_place(place_id)
+        # Checking.check_status_code(result_delete, 200)
+        # Checking.check_json_token(result_delete, ['status'])
+        # Checking.check_json_value(result_delete, 'status', 'OK')
+        #
+        #
+        # print("Method GET DELETE")
+        # result_get = GoogleMapsApi.get_new_place(place_id)
+        # Checking.check_status_code(result_get, 404)
+        # Checking.check_json_token(result_get, ['msg'])
+        # Checking.check_json_value(result_get, 'msg', "Get operation failed, looks like place_id  doesn't exists")
+        # Checking.check_json_search_word_in_value(result_get, 'msg', 'failed')
+        #
+
+        print("Testing uploading a list of characters from films with Darth Vader to a file has been completed successfully!!!")
---
+++ utils/cheking.py
@@ -0,0 +1,35 @@
+"""Methods for checking the responses of our requests"""
+import json
+
+
+class Checking:
+
+    """Methods for checking the status code"""
+    @staticmethod
+    def check_status_code(result, status_code):
+        assert status_code == result.status_code
+        print("Successfully!!! Status code:", result.status_code)
+
+    """Method for validating required fields in a request response"""
+    @staticmethod
+    def check_json_token(result, expected_value):
+        token = json.loads(result.text)
+        assert list(token) == expected_value
+        print("All fields are present")
+
+    """Method for checking the values of required fields in a response to a request"""
+    @staticmethod
+    def check_json_value(result, field_name, expected_value):
+        check = result.json()
+        check_info = check.get(field_name)
+        assert check_info == expected_value
+        print(f'The value of the {field_name} field is correct!!!')
+
+    """Method for checking the values of required fields in the request response for a given word"""
+
+    @staticmethod
+    def check_json_search_word_in_value(result, field_name, search_word):
+        check = result.json()
+        check_info = check.get(field_name)
+        assert search_word in check_info
+        print(f'The word {search_word} is present!!!')
